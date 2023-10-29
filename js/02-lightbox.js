52d2c0a45a813f9217a3289b063b74da8395215a
KZapal <kamil.zapal@o2.pl>
2023-10-29 19:02:42 +0100

    finish hw7?

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(`.gallery`);
+
+for (const item of galleryItems) {
+  const listItem = document.createElement(`li`);
+
+  const galleryItem = document.createElement(`a`);
+  galleryItem.classList.add(`gallery__item`);
+  galleryItem.href = `${item.original}`;
+  galleryItem.target = `_self`;
+
+  const galleryImage = document.createElement(`img`);
+  galleryImage.classList.add(`gallery__image`);
+  galleryImage.src = `${item.preview}`;
+  galleryImage.dataset.source = `${item.original}`;
+  galleryImage.alt = `${item.description}`;
+
+  gallery.append(listItem);
+  listItem.append(galleryItem);
+  galleryItem.append(galleryImage);
+}
+let lightbox = new SimpleLightbox(`.gallery__item`, {});
+
+gallery.addEventListener(`click`, (e) => {
+  e.preventDefault();
+});
